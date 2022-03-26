6bf38ae554b9c8da65346cb36a5dae081ed9cd3f
heiinhei911 <heiinhei911@gmail.com>
2022-03-26 16:55:43 -0400

    redesigned header and adjusted posts paddings

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,19 +5,20 @@ import { nanoid } from "nanoid";
 
 export default function Header({ darkMode, toggleTheme }) {
   return (
-    <div className="banner">
-      <h1 className="banner-title" key={nanoid()}>
-        Spacebook
-      </h1>
-      <p
-        className="banner-description"
-        style={{
-          borderBottom: `1px solid ${darkMode ? "#ffffff" : "#d3d3d3"}`,
-        }}
-        key={nanoid()}
-      >
-        powered by NASA's image API
-      </p>
+    <div
+      className="banner"
+      style={{
+        borderBottom: `1px solid ${darkMode ? "#ffffff" : "#d3d3d3"}`,
+      }}
+    >
+      <div className="banner-texts">
+        <h1 className="banner-title" key={nanoid()}>
+          Spacebook
+        </h1>
+        <p className="banner-description" key={nanoid()}>
+          powered by NASA's image API
+        </p>
+      </div>
       <div className="theme" onClick={toggleTheme} key={nanoid()}>
         <FontAwesomeIcon
           icon={darkMode ? faMoon : faLightbulb}
